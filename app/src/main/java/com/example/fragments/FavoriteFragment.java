3f8a45c00c877c95a00896fbf88ee47c1a383ae4
SynthiaAtieno <ondengsynthia222@gmail.com>
2022-07-30 22:42:32 +0300

    Added some new functionalities

--- app/src/main/java/com/example/fragments/FavoriteFragment.java
+++ app/src/main/java/com/example/fragments/FavoriteFragment.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.myapplication.R;
 
 public class FavoriteFragment extends Fragment {
 
