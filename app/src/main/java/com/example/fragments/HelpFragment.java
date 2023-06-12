b40e5843a708eaff49c9e3f1162b1b557f2f39c1
SynthiaAtieno <61385058+SynthiaAtieno@users.noreply.github.com>
2023-06-12 10:41:18 +0300

    Update HelpFragment.java

--- app/src/main/java/com/example/fragments/HelpFragment.java
+++ app/src/main/java/com/example/fragments/HelpFragment.java
@@ -13,7 +13,6 @@ import com.example.myapplication.R;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HelpFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class HelpFragment extends Fragment {
 
